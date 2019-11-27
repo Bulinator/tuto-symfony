ec903868a7da684bcbad7b5a3cdb01614bfc2f10
bulotge <bulotge@gisis.eu>
2019-11-27 16:26:47 +0100

    feat: Fixtures with references (for relations)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\BlogPost;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
@@ -12,23 +13,53 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        // get instance of user
+        $user = $this->getReference('user_admin');
+
         $blogPost = new blogPost();
         $blogPost->setTitle('A first post');
         $blogPost->setPublished(new \DateTime('2019-11-27 11:00:00'));
         $blogPost->setContent('New post fixtures');
-        $blogPost->setAuthor('Batman');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('batman-post-source');
 
         $manager->persist($blogPost); // save data
 
         $blogPost = new blogPost();
-        $blogPost->setTitle('A secondt post');
+        $blogPost->setTitle('A second post');
         $blogPost->setPublished(new \DateTime('2019-11-24 08:00:00'));
         $blogPost->setContent('New post');
-        $blogPost->setAuthor('Batman');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('batman-post-second');
 
         $manager->persist($blogPost); // save data
+
+        $manager->flush();
+    }
+
+    public function loadComments(ObjectManager $manager)
+    {
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@blog.com');
+        $user->setName('Batman Wayne');
+
+        $user->setPassword('secret1234#');
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user); // save data
 
         $manager->flush();
     }

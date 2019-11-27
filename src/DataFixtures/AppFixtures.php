d51525675f3e92e2ac3957e643dd1d4bc03be468
bulotge <bulotge@gisis.eu>
2019-11-27 16:46:54 +0100

    feat: Fixtures password encoding

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,9 +6,20 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\BlogPost;
 use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
@@ -55,7 +66,9 @@ class AppFixtures extends Fixture
         $user->setEmail('admin@blog.com');
         $user->setName('Batman Wayne');
 
-        $user->setPassword('secret1234#');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user, 'secret123#'
+        ));
 
         $this->addReference('user_admin', $user);
 

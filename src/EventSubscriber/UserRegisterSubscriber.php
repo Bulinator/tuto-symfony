a55bed6535e147b925f02e12a1b931ff8e6e362e
bulotge <bulotge@gisis.eu>
2020-01-06 14:16:17 +0100

    feat: Generating confirmation token when user signs-up

--- src/EventSubscriber/UserRegisterSubscriber.php
+++ src/EventSubscriber/UserRegisterSubscriber.php
@@ -4,43 +4,59 @@ namespace App\EventSubscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\User;
+use App\Security\TokenGenerator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class PasswordHashSubscriber implements  EventSubscriberInterface
+class UserRegisterSubscriber implements  EventSubscriberInterface
 {
     /**
      * @var UserPasswordEncoderInterface
      */
     private $passwordEncoder;
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    /**
+     * @var TokenGenerator
+     */
+    private $tokenGenerator;
+
+    /**
+     * UserRegisterSubscriber constructor.
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param TokenGenerator $tokenGenerator
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder, TokenGenerator $tokenGenerator)
     {
         $this->passwordEncoder = $passwordEncoder;
+        $this->tokenGenerator = $tokenGenerator;
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]
+            KernelEvents::VIEW => ['userRegistered', EventPriorities::PRE_WRITE]
         ];
     }
 
-    public function hashPassword(ViewEvent $event)
+    public function userRegistered(ViewEvent $event)
     {
         $user = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
-        if (!$user instanceof User || !in_array($method, [Request::METHOD_POST, Request::METHOD_PUT])) {
+        if (!$user instanceof User || !in_array($method, [Request::METHOD_POST])) {
             return;
         }
 
         // It's anUser, we need to hash password here
         $user->setPassword(
             $this->passwordEncoder->encodePassword($user, $user->getPassword())
+        );
+
+        $user->setConfirmationToken(
+            $this->tokenGenerator->getRandomSecureToken()
         );
     }
 }

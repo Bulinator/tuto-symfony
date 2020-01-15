bb80534f2c0e37b86f14ef4856bf25dd3fe7fe2b
bulotge <bulotge@gisis.eu>
2020-01-15 14:48:34 +0100

    fixed: dev test

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -58,7 +58,7 @@ use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
  * )
  * @ApiResource(
  *     attributes={
- *         "order"={"published": "DESC"}, "maximum_items_per_page"=5
+ *         "order"={"published": "DESC"}, "maximum_items_per_page"=5, "pagination_partial"=false
  *     },
  *     itemOperations={
  *         "get"={
